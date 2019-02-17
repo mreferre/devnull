76f34492ccb767bae9ae8541b1f03d4cc9dda3d3
Massimo Re Ferre <massimo@it20.info>
2019-02-17 20:35:22 +0100

    added EnvServiceProvider to app.modules.ts

--- yelb-ui/clarity-seed-newfiles/src/app/app.module.ts
+++ yelb-ui/clarity-seed-newfiles/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { environment } from '../environments/environment';
+import { EnvServiceProvider } from './env.service.provider';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import {NgxChartsModule} from '@swimlane/ngx-charts';
