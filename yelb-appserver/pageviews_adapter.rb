c4cacad1af817c58365486309704fb8688075fc6
Massimo Re Ferre <massimo@it20.info>
2019-02-18 15:21:09 +0100

    Cors

--- yelb-appserver/pageviews_adapter.rb
+++ yelb-appserver/pageviews_adapter.rb
@@ -1,6 +1,10 @@
 require_relative 'modules/pageviews'
 
 def pageviews_adapter(event:, context:)
+    headers 'Access-Control-Allow-Origin' => '*'
+    headers 'Access-Control-Allow-Headers' => 'Authorization,Accepts,Content-Type,X-CSRF-Token,X-Requested-With'
+    headers 'Access-Control-Allow-Methods' => 'GET,POST,PUT,DELETE,OPTIONS'
+    content_type 'application/json'
     $redishost = ENV['redishost']
     $port = 6379
     pageviewscount = pageviews()
