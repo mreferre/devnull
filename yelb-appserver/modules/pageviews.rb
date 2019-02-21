84457b959f21d3bcf8808b7c30fceb2827474602
Massimo Re Ferre <massimo@it20.info>
2019-02-21 19:48:23 +0100

    xvvd

--- yelb-appserver/modules/pageviews.rb
+++ yelb-appserver/modules/pageviews.rb
@@ -11,15 +11,15 @@ def pageviews()
                     }
                 }
                 pageviewsrecord = dynamodb.get_item(params)
-                pageviewscount = pageviewsrecord.item['pageviewscount']
-                pageviewscount += 1 
+                $pageviewscount = pageviewsrecord.item['pageviewscount']
+                $pageviewscount += 1 
                 params = {
                         table_name: $yelbddbcache,
                         key: {
                             counter: 'pageviews'
                         },
                         update_expression: 'set pageviewscount = :c',
-                        expression_attribute_values: {':c' => pageviewscount},
+                        expression_attribute_values: {':c' => $pageviewscount},
                         return_values: 'UPDATED_NEW'
                 }
                 pageviewrecord = dynamodb.update_item(params)
@@ -27,8 +27,8 @@ def pageviews()
                 redis = Redis.new
                 redis = Redis.new(:host => $redishost, :port => 6379)
                 redis.incr("pageviews")
-                pageviewscount = redis.get("pageviews")
+                $pageviewscount = redis.get("pageviews")
                 redis.quit()
         end
-        return pageviewscount
+        return $pageviewscount
 end
